c99d14b44aead56f19fba7e3d5bdab19962ce6e7
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-09-14 14:06:09 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Home from './components/Home.jsx'
+import logo from './images/logo-vapt-small.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="logo-container">
+          <img src={logo} alt='virtualapt_logo' className='home-logo' />
+        </div>
         <Home />
       </div>
     );
