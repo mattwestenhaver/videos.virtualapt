a1cf64bf5620ffbaf5735a9eeea0df172f2711eb
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-12-13 13:23:09 -0500

    search functionality

--- src/components/videos/EachVideo.jsx
+++ src/components/videos/EachVideo.jsx
@@ -11,8 +11,12 @@ class EachVideo extends React.Component {
     return (
       <div className="each-video" key={this.props.index}>
         {this.props.video.thumbnail
-          ? <a href={this.props.video.url} target="_blank"><img src={this.props.video.thumbnail} alt={this.props.video.title} /></a>
-          : <a href={this.props.video.url} target="_blank"><img src={noThumbnail} alt='no thumbnail available' /></a>
+          ? <a href={this.props.video.url} target="_blank">
+              <img src={this.props.video.thumbnail} alt={this.props.video.title} />
+            </a>
+          : <a href={this.props.video.url} target="_blank">
+              <img src={noThumbnail} alt='no thumbnail available' />
+            </a>
         }
         <h4><a href={this.props.video.url} target="_blank">{this.props.video.title}</a></h4>
       </div>
--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from 'semantic-ui-react'
+// import { Link } from 'react-router-dom'
+import { Button, Form, Input } from 'semantic-ui-react'
 import auth from '../../auth.js'
 import EachVideo from './EachVideo.jsx'
 
@@ -10,8 +10,20 @@ class Videos extends React.Component {
     super(props)
     this.state = {
       videos: [],
-      filter: 'date'
+      filter: 'date',
+      videoSearch: '',
+      results: []
     }
+  }
+
+  searchVideos(e, { name, value }) {
+    var searchResults = []
+    this.state.videos.forEach(video => {
+      if(video.title.toLowerCase().includes(value !== '' ? value.toLowerCase() : value)) {
+        searchResults = [...searchResults, video]
+      }
+    })
+    this.setState({ [name]: value, results: searchResults })
   }
 
   loadVideos() {
@@ -35,44 +47,82 @@ class Videos extends React.Component {
   }
 
   render() {
+    
+    const { videoSearch } = this.state
+
     return (
       <div>
         <div className="videos-container">
           <div className="filter">
             <Button onClick={this.nameFilter.bind(this)}>Name</Button> | <Button onClick={this.dateFilter.bind(this)}>Upload Date</Button>
           </div>
+          <div className="search-input">
+            <Form.Field control={Input} name='videoSearch' value={videoSearch} placeholder='Search...' onChange={this.searchVideos.bind(this)} />
+          </div>
           <div className="videos-row">
-            {this.state.videos.length > 0
-              ? this.state.filter === 'name'
-                ? this.state.videos.sort((a,b) => {
-                    var videoA = a.title.toLowerCase()
-                    var videoB = b.title.toLowerCase()
-                    if (videoA < videoB) {
-                      return -1
-                    } else if (videoA > videoB) {
-                      return 1
-                    } else {
-                      return 0
-                    }
-                  }).map((video, index) => {
-                    return <EachVideo video={video} index={index} />
-                  })
-                : this.state.videos.sort((a,b) => {
-                    var videoA = a.uploadDate
-                    var videoB = b.uploadDate
-                    if (videoA < videoB) {
-                      return 1
-                    } else if (videoA > videoB) {
-                      return -1
-                    } else {
-                      return 0
-                    }
-                  }).map((video, index) => {
-                    return <EachVideo video={video} index={index} />
-                  })
-              : <div>
-                  <h2>Loading Videos...</h2><br />
-                </div>
+            {this.state.videoSearch === null || this.state.videoSearch === ''
+              ? this.state.videos.length > 0
+                  ? this.state.filter === 'name'
+                    ? this.state.videos.sort((a,b) => {
+                        var videoA = a.title.toLowerCase()
+                        var videoB = b.title.toLowerCase()
+                        if (videoA < videoB) {
+                          return -1
+                        } else if (videoA > videoB) {
+                          return 1
+                        } else {
+                          return 0
+                        }
+                      }).map((video, index) => {
+                        return <EachVideo video={video} index={index} key={video._id} />
+                      })
+                    : this.state.videos.sort((a,b) => {
+                        var videoA = a.uploadDate
+                        var videoB = b.uploadDate
+                        if (videoA < videoB) {
+                          return 1
+                        } else if (videoA > videoB) {
+                          return -1
+                        } else {
+                          return 0
+                        }
+                      }).map((video, index) => {
+                        return <EachVideo video={video} index={index} key={video._id} />
+                      })
+                  : <div>
+                      <h2>Loading Videos...</h2><br />
+                    </div>
+              : this.state.results.length > 0
+                ? this.state.filter === 'name'
+                  ? this.state.results.sort((a,b) => {
+                      var videoA = a.title.toLowerCase()
+                      var videoB = b.title.toLowerCase()
+                      if (videoA < videoB) {
+                        return -1
+                      } else if (videoA > videoB) {
+                        return 1
+                      } else {
+                        return 0
+                      }
+                    }).map((video, index) => {
+                      return <EachVideo video={video} index={index} key={video._id} />
+                    })
+                  : this.state.results.sort((a,b) => {
+                      var videoA = a.uploadDate
+                      var videoB = b.uploadDate
+                      if (videoA < videoB) {
+                        return 1
+                      } else if (videoA > videoB) {
+                        return -1
+                      } else {
+                        return 0
+                      }
+                    }).map((video, index) => {
+                      return <EachVideo video={video} index={index} key={video._id} />
+                    })
+                : <div>
+                    <h2>No results match your search criteria...</h2><br />
+                  </div>
             }
           </div>
         </div>
